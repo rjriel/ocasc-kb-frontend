c29ddb08f3e23189491edd1b1323bc54001af685
philxchen <cxuankai@outlook.com>
2018-10-14 09:07:40 -0400

    Update index entry point

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { UpdatecontentComponent } from './updatecontent/updatecontent.component'
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'updatecontent', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'updatecontent', component: UpdatecontentComponent },
   { path: 'login', component: LoginComponent },
   { path: 'entries', component: EntriesComponent }
